7a37f3eb5fad1b12ec0c54b0d68b58ef36e3b3d4
ruslan0515015 <ruslaniosdev@gmail.com>
2024-04-19 15:11:11 +0300

    Configuring Navbar

--- NewsApp/NewsViewController.swift
+++ NewsApp/NewsViewController.swift
@@ -1,12 +1,28 @@
 import UIKit
 
-class NewsViewController: UIViewController {
-
+class NewsViewController: UINavigationController {
+// MARK: - Properties:
+  
+  // MARK: - Private Properties:
+  
+  // MARK: - LifeCycle:
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.view.backgroundColor = .red
+    self.view.backgroundColor = .white
+    configureNavbar()
   }
-
-
 }
 
+// MARK: - Private Methods:
+extension NewsViewController {
+  private func configureNavbar() {
+    self.navigationBar.barTintColor = .white
+    self.navigationBar.isTranslucent = false
+    self.navigationController?.title = "news"
+  }
+}
+
+// MARK: - Objc-Methods:
+extension NewsViewController {
+  
+}
