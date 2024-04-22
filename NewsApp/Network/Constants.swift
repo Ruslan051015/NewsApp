bbe2642e488b92739f96907c02cd15e6919990ba
ruslan0515015 <ruslaniosdev@gmail.com>
2024-04-22 16:59:18 +0300

    FreshToken Added

--- NewsApp/Network/Constants.swift
+++ NewsApp/Network/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 struct Constants {
-  static let apiKey = "e48b2fb44d0efc580bc52dbe116e0d6a"
+  static let apiKey = "658f2dbf43487a0aaa57b62317b9871d"
   static let baseURLString = "https://gnews.io/api/v4/top-headlines"
 }
